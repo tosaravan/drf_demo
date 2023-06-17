d1549315e70a7ecc5c6d5cdc15ea8487ff96b790
saravanan <tosaravan@yahoo.co.uk>
2023-06-17 08:09:16 +0100

    student model added

--- apis/models.py
+++ apis/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     fullname = models.CharField(max_length=200)
-    email = models.EmailField
+    email = models.CharField(max_length=200)
 
     def __str__(self):
         return self.fullname
