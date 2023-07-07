459e8ae4af220d31d65de81ab50a04daccb45953
saravanan <tosaravan@yahoo.co.uk>
2023-07-07 06:14:33 +0100

    tests for the apis

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class BasicTest(APITestCase):
+    def test_add_get_drinks(self):
+        data = {
+            "name": "tea",
+            "description": "milk drink"}
+
+        # post / add drink
+        self.client.post("/drinks/", data)
+
+        # get api to fetch the drinks
+        response = self.client.get("/drinks/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()[0]["name"], "tea")
+        self.assertEqual(response.json()[0]["description"], "milk drink")
